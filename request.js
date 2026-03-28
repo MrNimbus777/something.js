39eed938a3c02b470ee954485076bbac2caa4301
MrNimbus777 <114327199+MrNimbus777@users.noreply.github.com>
2026-03-28 20:00:13 +0200

    Update request.js
    Added:
    - more body duntions
    - timeout & abortController
    - retry mechanics

--- request.js
+++ request.js
@@ -1,10 +1,24 @@
+export function setLimitedInterval(callback = () => {}, delay = 1000, times = 1){
+    const interval = setInterval(() => {
+        if(times-- <= 0) {
+            clearInterval(interval);
+            return;
+        }
+        callback();
+    }, delay);
+    return interval;
+}
 class RequestBuilder{
     constructor(method, url, headers){
         this._url = url;
         this._method = method;
         this._headers = headers;
-        this._body = null;
+        this._body = undefined;
+        this._isRead = false;
+        this._signal = undefined;
+        this._retry = undefined;
     }
+    
     header(key, value) {
         this._headers[key] = value;
         return this;
@@ -25,18 +39,70 @@ class RequestBuilder{
         this._body = data;
         return this;
     }
+    jsonBody(obj){
+        this.body(JSON.stringify(obj));
+        this.contentType("application/json");
+        return this;
+    }
+    fileBody(file){
+        this.body(file);
+        this.contentType(file.type);
+        return this;
+    }
+    abortController(controller){
+        this._signal = controller.signal;
+        return this;
+    }
+    timeout(ms = 5000){
+        const controller = new AbortController();
+        this.abortController(controller);
+        setTimeout(controller.abort, ms);
+        return this;
+    }
+    retry(times = 1, delay = 3000){
+        this._retry = {times: times, delay: delay};
+        return this;
+    }
     async readRaw() {
-        const res = await fetch(this._url, {
-            method: this._method,
-            headers: this._headers,
-            body: this._body
-        });
-        return res;
+        if(this._isRead) throw new Error("Cannot send a request twice!");
+        this._isRead = true;
+
+        const retries = this._retry?.times ?? 0;
+        const delay = this._retry?.delay ?? 0;
+
+        let attempt = 0;
+
+        while (true) {
+            try {
+                const res = await fetch(this._url, {
+                    method: this._method,
+                    headers: this._headers,
+                    body: this._body,
+                    signal: this._signal
+                });
+                if (res.status >= 500 && attempt < retries) {
+                    attempt++;
+                    await (new Promise(resolve => setTimeout(resolve, delay)));
+                    continue;
+                }
+                
+                return res;
+            } catch (err) {
+                // Retry on network errors
+                if (attempt < retries && err.name !== "AbortError") {
+                    attempt++;
+                    await (new Promise(resolve => setTimeout(resolve, delay)));
+                    continue;
+                }
+
+                throw err;
+            }
+        }
     }
     async read(callback = undefined) { // callback: (res) => {...};
         const res = await this.readRaw();
         
-        if(callback){
+        if(callback && typeof callback === 'function'){
             return callback(res);
         }
         
